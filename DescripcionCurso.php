4e9ac9cd9ef6ee8189f0dad21a07d0b5bbab204f
Ang27180 <angelicaperezpadilla27@gmail.com>
2025-07-07 20:57:27 -0500

    Add files via upload

---
+++ DescripcionCurso.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Curso de Repostería</title>
+
+  <!-- Bootstrap y Font Awesome -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+  <!-- Tu CSS personalizado -->
+  <link rel="stylesheet" href="css/descripcioncurso.css">
+</head>
+<body>
+
+<div class="card">
+  <img src="img/reposteria.jpg" alt="Repostería Creativa" />
+  <div class="card-content">
+    <div class="title">Repostería Creativa <span class="rating">★ 5.0</span></div>
+    <div class="description">🍰 Domina el arte de la repostería con técnicas profesionales para crear postres irresistibles.</div>
+    <div class="price">$295.000</div>
+    <div class="plus" onclick="mostrarModal()">+</div>
+  </div>
+</div>
+
+
+
+
+
+</body>
+</html>
